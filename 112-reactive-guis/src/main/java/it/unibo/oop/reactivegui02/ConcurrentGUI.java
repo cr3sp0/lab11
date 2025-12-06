a41eefe4458a700a31c18064c2e58212d3584d85
Alessio Crespolini <crespolini.alessio@gmail.com>
2025-12-06 13:33:57 +0100

    excercise 112 done

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,7 +1,15 @@
 package it.unibo.oop.reactivegui02;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import it.unibo.oop.JFrameUtil;
+
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Second example of reactive GUI.
@@ -10,4 +18,82 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private final JLabel display = new JLabel();
+
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        final JButton upButton = new JButton("count");
+        panel.add(upButton);
+        final JButton downButton = new JButton("countdown");
+        panel.add(downButton);
+        final JButton exit = new JButton("stop");
+        panel.add(exit);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+
+        upButton.addActionListener(e -> {
+            if (!agent.getCountingStatus()) {
+                agent.changeCounting();
+            }
+        });
+
+        downButton.addActionListener(e -> {
+            if (agent.getCountingStatus()) {
+                agent.changeCounting();
+            }
+        });
+
+        exit.addActionListener(e -> {
+            agent.stopCounting();
+            upButton.setEnabled(false);
+            downButton.setEnabled(false);
+        });
+    }
+
+    private final class Agent implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean upDown = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    if (upDown) {
+                        final var nextText = Integer.toString(this.counter);
+                        SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                        this.counter++;
+                    } else {
+                        final var nextText = Integer.toString(this.counter);
+                        SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                        this.counter--;
+                    }
+
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    System.out.println("exeption occured: " + ex);
+                }
+            }
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void changeCounting() {
+            this.upDown = !this.upDown;
+        }
+
+        public boolean getCountingStatus() {
+            return this.upDown;
+        }
+
+    }
 }
